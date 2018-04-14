22f52662e08108244e0b20a88d1670a2b1fd39d8
Luoan Tang <luoant@uw.edu>
2018-04-13 22:29:43 -0700

    finish some parts

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,38 +2,39 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data(USPersonalExpenditure)
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+USPex <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(USPex)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(USPex)
 
 # Add a column "category" to your data frame that contains the rownames
-
+USPex$category <- rownames(USPex)
 
 # How much money was spent on personal care in 1940?
-
+USPex['Personal Care', 'X1940']
 
 # How much money was spent on Food and Tobacco in 1960?
+USPex['Food and Tobacco', 'X1960']
 
 
 # What was the highest expenditure category in 1960?
-
+USPex$category[USPex$X1960 == max(USPex$X1960)]
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
